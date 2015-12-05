d438819a178c1351bafd7ccaec9007134443fa34
Sebastien Saez <monsieursaez@gmail.com>
2015-12-05 19:41:01 +0100

    improved layout

--- labo5.cpp
+++ labo5.cpp
@@ -11,13 +11,13 @@
  
 
  Remarque(s) : - Le programme gère les exceptions de la saisie utilisateur.
-               - La saisie est de la forme: moisDebut anneeDebut moisFin anneeFin
-                 situer entre BORNE_ANNEE_MINIMALE, BORNE_ANNEE_MAXIMAL.
-               - Les valeurs du programme peuvent facilement être modifié 
+               - La saisie est de la forme: moisDebut(mm) anneeDebut(yyyy) moisFin anneeFin
+                 située entre BORNE_ANNEE_MINIMALE, BORNE_ANNEE_MAXIMAL.
+               - Les valeurs du programme peuvent facilement être modifiées 
                  (constantes prévues à cet effet).
                - Le programme est structuré en fonctions
                - Le programme tient compte des années bissextiles.
-               - A la fin de la première exécution l'utilisateur a la possibilité,
+               - A la fin de la première exécution l'utilisateur a la possibilité
                  d'afficher un nouveau calendrier ou de quitter l'application.
    
  Compilateur : g++ 5.2.0
@@ -107,7 +107,8 @@ void afficheCalendrier(int mois1, int mois2, int annee1, int annee2)
                   setw(LARGEUR_COLONNE)) << setfill(' ') << compteur;
          index++;
          compteur++;
-         if (index > DIMANCHE) {//début semaine
+         if (index > DIMANCHE) 
+         {//début semaine
             cout << endl;
             index = LUNDI;
          }
